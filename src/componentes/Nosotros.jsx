c9246f7dde4cefa67f67ac4ded68ddb680a79658
pablo.cristini <pablitoocristini@gmail.com>
2023-05-23 15:38:21 -0300

    tercer commit

--- src/componentes/Nosotros.jsx
+++ src/componentes/Nosotros.jsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react"
+
 const Nosotros = () => {
+
+
+    useEffect(() => {
+        window.addEventListener("click", () => {
+            console.log("click")
+        })
+
+        return (
+            window.removeEventListener("click", () => {
+                console.log(" cierre click")})
+        )
+    }, [])
+
     return ( 
         <div>
             <h3>Que somos? </h3>
